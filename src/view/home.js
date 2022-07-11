380918aa3e358ef1bfa6fcda141b492623dc0422
Bellasacc <bellasacc10@gmail.com>
2022-07-11 11:33:55 -0500

    estilos de la version mobile del nav de la vista home

--- src/view/home.js
+++ src/view/home.js
@@ -1,13 +1,13 @@
 export default () => {
   const viewHome = `<section class="home-page">
     <header>
-      <nav>
+      <nav class="nav-menu">
         <ul class="menu-left">
           <li>
-            <a href=""><img class="img-profile" src="img/perfiltwitchtter.png"/>><a>
+            <a href="#"><img class="img-profile" src="img/perfiltwitchtter.png" alt="perfil"></a>
           </li>
-          <li>
-            <a href=""><img class="img-top hidden" src="img/top.png"/><a>
+          <li class="hidden" >
+            <a href=""><img class="img-top" src="img/top.png" alt="ir arriba"></a>
           </li>
           <li class="title-home">TWITCHTTER</li>
           <li class="mentions hidden">
@@ -19,13 +19,17 @@ export default () => {
           <input type="search" id="searchBar" placeholder="Buscar en Twitchtter" />
         </li>
         <li>
-          <a href=""><img class="game-over" src="img/gameover.png"/>><a>
+          <a href=""><img class="game-over" src="img/gameover.png" alt="icono logout"></a>
         </li>
         <li class="version hidden">V.1.1</li>
         </ul>
       </nav>
     </header>
-      <a href="#/login"><button type="button" class="btn-logOut btn-general">Salir</button></a>
+    <section class="container-main">
+
+    </section>
   </section>`;
   return viewHome;
 };
+
+/* <a href="#/login"><button type="button" class="btn-logOut btn-general">Salir</button></a> */
